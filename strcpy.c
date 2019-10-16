26c2bb945c92a3f692d50e5b6121a4e056d41b70
Vivek Yadav <vivekyadav1207731111@gmail.com>
2019-10-16 22:01:33 +0530

    Create strcpy.c

---
+++ strcpy.c
@@ -0,0 +1,12 @@
+/* 
+
+  Copy String :
+  
+    char * MyStrcpy(destination,source)
+    
+    i.e 
+    > copy source string into destination and print into main file.
+    > Do with temp variable
+    > try to eleminate this temp variable
+
+*/
