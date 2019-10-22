3cbbddc5d17b72bb738e8e29395643e858ada1de
casabiancayadav <39937154+casabiancayadav@users.noreply.github.com>
2019-10-23 03:31:00 +0530

    Update strcpy.c

--- strcpy.c
+++ strcpy.c
@@ -9,4 +9,30 @@
     > Do with temp variable
     > try to eleminate this temp variable
 
-*/
+*/#include<stdio.h>
+
+void COPY(char D[], char S[])
+     {
+     	int i;
+     
+     	for(i=0;S[i]!='\0';i++)
+    
+     	{
+     		D[i]=S[i];
+		 }
+		 
+		 
+	 }
+   
+    int main()
+   {
+   		char S[100];
+   	    char D[100];
+   	printf("ENTER THE NAME\n");
+   	scanf("%s",&S);
+     	
+   	COPY(D,S);
+   	printf("\nCopy string=%s",D);
+   	
+   }
+   
